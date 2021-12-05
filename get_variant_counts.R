3de9beaf1deb8886b1d13f65c6a30c9220ec4b63
Daniel Gatti <dan.gatti@jax.org>
2021-12-05 13:37:39 -0500

    First commit

---
+++ get_variant_counts.R
@@ -0,0 +1,112 @@
+################################################################################
+# Pileup the BAM files and intersect them with the Sanger variants that occur
+# in transcripts.
+#
+# Daniel Gatti
+# dan.gatti@jax.org
+# 2021-12-05
+################################################################################
+
+##### LIBRARIES #####
+
+library(Rsamtools)
+
+source('check_file_access.R')
+source('read_sanger_vars.R')
+
+##### VARIABLES #####
+
+args = commandArgs(trailing = TRUE)
+
+if(length(args) != 3) {
+
+  stop(paste('ERROR: Three arguments required: bam_file, sanger_file, out_prefix'))
+
+} # if(length(args) != 3)
+
+bam_file    = args[1]
+sanger_file = args[2]
+out_prefix  = args[3]
+
+# Test code
+#bam_file   = '/fastscratch/dgatti/rsem/Sample_100-GES15-05705.STAR.genome.bam_sorted'
+#sanger_file = '/projects/compsci/USERS/dgatti/data/gbrs_snp/sanger_transcript_snps_indels_ens102_b38.tsv.bgz'
+#out_prefix  = '/fastscratch/dgatti/counts/Sample_100-GES15-05705'
+
+check_file_access(bam_file,            4)
+check_file_access(sanger_file,         4)
+check_file_access(dirname(out_prefix), 2)
+
+##### PROGRAM #####
+
+# Open the Sanger file.
+sanger = TabixFile(sanger_file)
+
+header = headerTabix(sanger)
+
+# Get the chromosomes.
+chr_names = header$seqnames
+chr_names = chr_names[order(as.numeric(chr_names))]
+
+# Get column names.
+cn = strsplit(sub('^#', '', header$header[length(header$header)]), '\t')[[1]]
+
+# Process one chromosome at a time.
+for(chr in chr_names) {
+
+  print(paste('Chromosome', chr))
+  
+  # Read in Sanger variants on this chromosome.
+  gr = GRanges(seqnames = chr, IRanges(start = 0, end = 200e6))
+  vars = read_sanger_vars(sanger, gr)
+  
+  # Retain only SNPs for now.
+  vars = subset(vars, indel == FALSE)
+  
+  print(paste('Found', nrow(vars), 'variants on chr', chr))
+  
+  # Pileup BAM file on this chromosome.
+  # TBD: Switch over to Rsamtools::PileupFiles?
+  sb_param = ScanBamParam(what = 'seq', which = gr)
+  pu_param = PileupParam()
+  
+  pu = pileup(bam_file, scanBamParam = sb_param, pileupParam = pu_param)
+  pu = GRanges(pu$seqnames, IRanges(pu$pos, width = 1), strand = pu$strand,
+               mcols = pu[,c('nucleotide', 'count')])
+  colnames(mcols(pu)) = sub('^mcols\\.', '', colnames(mcols(pu)))
+  
+  vars = GRanges(vars$chr, IRanges(vars$pos, width = 1),
+                 mcols = vars[,c('marker', 'ref', 'alt', 'tx_id', 'tx_strand', 'gene_id',
+                                'indel', 'A_J', 'C57BL_6J', '129S1_SvImJ', 
+                                'NOD_ShiLtJ', 'NZO_HlLtJ', 'CAST_EiJ', 'PWK_PhJ',
+                                'WSB_EiJ')])
+  colnames(mcols(vars)) = sub('^mcols\\.', '', colnames(mcols(vars)))
+
+  ol = findOverlaps(pu, vars)
+  
+  pu = as.data.frame(pu[queryHits(ol)])
+  pu = pu[,c('seqnames', 'start', 'strand', 'nucleotide', 'count')]
+  vars = as.data.frame(vars[subjectHits(ol)])
+  colnames(vars) = sub('^X', '', colnames(vars))
+  vars = vars[,c('marker', 'ref', 'alt', 'tx_id', 'tx_strand', 'gene_id',
+                 'indel', 'A_J', 'C57BL_6J', '129S1_SvImJ', 'NOD_ShiLtJ', 
+                 'NZO_HlLtJ', 'CAST_EiJ', 'PWK_PhJ', 'WSB_EiJ')]
+  counts = cbind(pu, vars)
+    
+  rm(pu, vars)
+  gc()
+  
+  print(paste('Writing counts for', basename(out_prefix)))  
+  
+  counts = counts[,c('marker', 'seqnames', 'start', 'ref', 'alt', 'strand', 
+                 'nucleotide', 'count', 'tx_id', 'tx_strand', 'gene_id',
+                 'indel', 'A_J', 'C57BL_6J', '129S1_SvImJ', 'NOD_ShiLtJ', 
+                 'NZO_HlLtJ', 'CAST_EiJ', 'PWK_PhJ', 'WSB_EiJ')]
+  colnames(counts)[2:3] = c('chr', 'pos')
+  
+  out_file = paste0(out_prefix, '_counts.csv')
+  write.table(counts, file = out_file, quote = FALSE, sep = ',', row.names = FALSE,
+              col.names = chr == '1', append = chr != '1')
+  
+} # for(chr)
+
---
+++ get_variant_counts.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+#SBATCH --qos dev
+#SBATCH --partition dev
+#SBATCH --nodes 1 # number of nodes
+#SBATCH --ntasks 1 # number of cores
+#SBATCH --mem 8G # memory pool for all cores
+#SBATCH --time 0-1:00 # time (D-HH:MM)
+#SBATCH --array=00-19
+
+################################################################################
+# Pileup counts in genome BAM files (sorted & indexed) at the Sanger transcript
+# variants. Output a file with counts and loci.
+# Daniel Gatti
+# dan.gatti@jax.org
+# 2021-12-05
+################################################################################
+
+##### VARIABLES #####
+
+# Full path to directory containing BAM files.
+BAM_DIR=/fastscratch/dgatti/rsem
+
+# Full path of BAM files in BAM_DIR.
+BAM_FILES=(`ls ${BAM_DIR}/*_sorted`)
+
+# Full path to current BAM file.
+BAM_FILE=${BAM_FILES[${SLURM_ARRAY_TASK_ID}]}
+
+# Path to Sanger transcript SNP/INDEL file.
+SANGER_FILE=/projects/compsci/USERS/dgatti/data/gbrs_snp/sanger_transcript_snps_indels_ens102_b38.tsv.bgz
+
+# Sample ID for this sample.
+SAMPLE=${BAM_FILE/${BAM_DIR}\//}
+SAMPLE=${SAMPLE/.STAR.genome.bam_sorted/}
+
+# Full path to output directory.
+OUT_DIR=/fastscratch/dgatti/counts
+
+# Output file prefix for this sample.
+OUT_FILE=${OUT_DIR}/${SAMPLE}
+
+# R Bioconductor container.
+CONTAINER=~/containers/bioconductor.sif
+
+# R script to run.
+RSCRIPT=get_variant_counts.R
+
+##### PROGRAM #####
+
+mkdir -p ${OUT_DIR}
+
+touch ${BAM_DIR}/*
+
+module load singularity
+
+echo Processing ${SAMPLE}
+
+singularity exec ${CONTAINER} Rscript ${RSCRIPT} ${BAM_FILE} ${SANGER_FILE} ${OUT_FILE}
+
